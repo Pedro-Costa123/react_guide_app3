2a718e06ccb5e34961faebfff6579a64c670771b
Pedro-Costa123 <pedrocostaalves@live.com.pt>
2022-12-05 17:51:14 +0000

    Section 10 : 114 + joined 2 similar apps in one rep

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import './index.css';
-import App from './App';
+import './index2.css';
+import App from './V2/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
