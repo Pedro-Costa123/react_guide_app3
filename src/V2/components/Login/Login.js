202a2e84d2f10b6c5ce3a90cad0410659d7fff69
Pedro-Costa123 <pedrocostaalves@live.com.pt>
2022-12-08 15:20:14 +0000

    Section 10 : 124

--- src/V2/components/Login/Login.js
+++ src/V2/components/Login/Login.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect, useReducer } from "react";
+import React, { useState, useEffect, useReducer, useContext } from "react";
 
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
 import Button from "../UI/Button/Button";
+import AuthContext from "../../context/auth-context";
 
 const emailReducer = (state, action) => {
   if (action.type === "USER_INPUT") {
@@ -85,8 +86,10 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value);
+    ctx.onLogin(emailState.value, passwordState.value);
   };
+
+  const ctx = useContext(AuthContext);
 
   return (
     <Card className={classes.login}>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import './index2.css';
-import App from './V2/App';
+import "./index2.css";
+import App from "./V2/App";
+import { AuthContextProvider } from "./V2/context/auth-context";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <AuthContextProvider>
+    <App />
+  </AuthContextProvider>
+);
